d0eb5b779b44fd989d037c0af33354286b548bf4
devbyjonah <jonahwagner231@gmail.com>
2022-11-15 13:48:53 -0800

    connected to mongodb

--- server.js
+++ server.js
@@ -1,15 +1,21 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const MongoClient = require('mongodb').MongoClient
+const dotenv = require('dotenv')
+dotenv.config()
 
 const app = express()
 const PORT = 8000
 
+const connectionString = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.uxd8j5d.mongodb.net/?retryWrites=true&w=majority`
+
 app.use(bodyParser.urlencoded({ extended: true }))
 
-MongoClient.connect('', (err, client) => {
-	
-})
+MongoClient.connect(connectionString)
+	.then(client => {
+		console.log('connected to DB')
+	})
+	.catch(error => console.error(error))
 
 app.get('/', (req, res) => {
 	res.sendFile(__dirname + '/index.html')
