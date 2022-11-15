8c2c54e1a8cee1f9f7fb939f799bade4cf6ec040
devbyjonah <jonahwagner231@gmail.com>
2022-11-14 21:16:03 -0800

    nodemon setup

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ const express = require('express')
 const app = express()
 const PORT = 8000
 
+app.get('/', (req, res) => {
+	res.sendFile(__dirname + '/index.html')
+})
+
 app.listen(process.env.PORT || PORT, () => {
 	console.log(`The server is running on port ${PORT}`)
 })
