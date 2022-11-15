90c1141ad9693385135ed65922f4666ad7cc024c
devbyjonah <jonahwagner231@gmail.com>
2022-11-14 21:43:46 -0800

    body-parser setup

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+const MongoClient = require('mongodb').MongoClient
+
 const app = express()
 const PORT = 8000
 
+app.use(bodyParser.urlencoded({ extended: true }))
+
+MongoClient.connect('', (err, client) => {
+	
+})
+
 app.get('/', (req, res) => {
 	res.sendFile(__dirname + '/index.html')
+})
+
+app.post('/notes', (req, res) => {
+	console.log(req.body)
 })
 
 app.listen(process.env.PORT || PORT, () => {
